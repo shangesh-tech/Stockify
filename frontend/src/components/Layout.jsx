95e3bcb94f61298691d19c92fcfc8bcfa4cdb278
Shangesh S <shangesh2006@gmail.com>
2025-02-08 14:50:24 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -99,7 +99,7 @@ const Layout = ({ children }) => {
                 to="https://www.tickertape.in/market-mood-index"
                 className="flex justify-center items-center gap-2"
               >
-                <MdMood size={30}/>
+                <MdMood size={20}/>
                 <span>Fear/Greed Index</span>
               </Link>
             </li>

c5f0862fc53d636932ed2dd9c6468db39d9ef507
shangesh-tech <shangesh2006@gmail.com>
2025-02-08 14:45:21 +0530

    feat:Final Update

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home"; // Your landing page (without Outlet)
+import Home from "../pages/Home"; 
 import Login from "../pages/Login";
 import SignUp from "../pages/Signup";
 import Dashboard from "../pages/Dashboard";
 import Portfolio from "../pages/Portfolio";
 import StocksAnalysis from "../pages/StocksAnalysis";
 import ProtectedRoute from "../components/ProtectedRoute";
-import PortfolioTest from "../PortfolioTest";
 import Layout from "../components/Layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />, // Home page remains separate (no Outlet required)
+    element: <Home />, 
   },
   {
     path: "/login",

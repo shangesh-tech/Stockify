c939e210316eb8f3b9047d6625c7988210a0bdc8
Shangesh S <shangesh2006@gmail.com>
2025-04-22 09:53:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import Home from "./pages/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const measurementId = "G-SFZVX02FV7";
+ReactGA.initialize(measurementId);
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send("pageview", { page_path: location.pathname });
+  }, [location]);
+
   return (
     <>
       <Home />

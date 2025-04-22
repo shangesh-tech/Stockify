dd8f6427ef6bcc89c929a8858b4b54b52afe31ee
Shangesh S <shangesh2006@gmail.com>
2025-04-22 11:01:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
-import Home from "./pages/Home";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Home from './pages/Home';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const measurementId = "G-SFZVX02FV7";
+const measurementId = 'G-SFZVX02FV7';
 ReactGA.initialize(measurementId);
 
-function App() {
+const TrackPageView = () => {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.send("pageview", { page_path: location.pathname });
+    ReactGA.send({
+      hitType: 'pageview',
+      page_path: location.pathname + location.search,
+    });
   }, [location]);
 
+  return null;
+};
+
+function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <TrackPageView />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
